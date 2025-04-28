482547bdc5e6c3a1b672fc070812a87a53bb1d22
Ganesh Kambli <gkambli70@ gmail.com>
2025-04-28 19:38:58 +0530

    changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -70,3 +70,29 @@ for (let i = 0, len = revealDelayElements.length; i < len; i++) {
 
 window.addEventListener("scroll", reveal);
 window.addEventListener("load", reveal);
+
+
+/**
+ * SCROLL TO SECTION ON CLICK
+ */
+
+const navLinks = document.querySelectorAll("[data-nav-link]");
+
+const scrollToSection = function () {
+  const targetId = this.getAttribute("href").substring(1);
+  const targetElement = document.getElementById(targetId);
+
+  if (targetElement) {
+    window.scrollTo({
+      top: targetElement.offsetTop - header.offsetHeight, // Adjust for fixed header height
+      behavior: "smooth"
+    });
+
+    // Close mobile nav if open
+    if (navbar.classList.contains("active")) {
+      toggleNavbar();
+    }
+  }
+}
+
+addEventOnElements(navLinks, "click", scrollToSection);
